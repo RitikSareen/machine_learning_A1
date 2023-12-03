f8e768abcde4b1e8fe74e8d2d6881306ef34fc00
RitikSareen <152048463+RitikSareen@users.noreply.github.com>
2023-12-03 21:57:51 +0700

    update

--- app.py
+++ app.py
@@ -1,41 +1,3 @@
-# from flask import Flask,render_template,request
-# import pickle
-# import numpy as np
-
-# model=pickle.load(open('A1_Car.pkl','rb'))
-
-# app=Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     transmission = request.form['Transmission']
-#     fuel = request.form['Fuel']
-#     driven = float(request.form['driven'])
-#     year = float(request.form['year'])
-#     engine = float(request.form['Engine'])
-
-#     #Process the input as per your requirements
-#     fuel = 1 if fuel.lower() == 'petrol' else 0
-#     transmission = 1 if transmission.lower() == 'automatic' else 0
-
-#     # Take the log of driven
-#     driven = np.log(driven)
-
-#     # Prepare the input for prediction
-#     input_data = np.array([[fuel,transmission, driven, year, engine]])
-
-#     # Make prediction using the loaded model
-#     prediction = model.predict(input_data)
-
-
-#     return render_template('final.html',data=prediction)
-
-# if __name__ =='__main__':
-#     app.run(debug=True)
 
 
 
@@ -73,7 +35,9 @@ def predict():
 
     # Make prediction using the loaded model
     prediction = model.predict(input_data)
-    print("this is the prediction" ,prediction)
+    prediction=np.exp(prediction)
+
+    # print("this is the prediction" ,prediction)
     # Render the prediction result on the index.html template
     return render_template('index.html', prediction=prediction[0])
 
